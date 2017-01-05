e4a592b80d5c3725b01ebef517fcadfdf971b0c4
Raywill <hustos@qq.com>
2017-01-05 12:17:01 +0800

    more data

--- animate.sh
+++ animate.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd /home/wwwroot/www.reactshare.cn/air/images
 convert -delay 100 -loop 0 *.png animation.gif
+convert -delay 20 -loop 0 *.png animation-fast.gif
 #convert -loop 0 -reverse animation-tmp.gif animation.gif
 #rm -f animation-tmp.gif
 
