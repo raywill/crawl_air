5e0000f61b2adee75e1b512e01dec952470c2136
Raywill <hustos@qq.com>
2017-01-05 12:30:46 +0800

    增加视频支持的脚本

--- animate.sh
+++ animate.sh
@@ -2,7 +2,8 @@
 cd /home/wwwroot/www.reactshare.cn/air/images
 convert -delay 100 -loop 0 *.png animation.gif
 convert -delay 20 -loop 0 *.png animation-fast.gif
-#convert -loop 0 -reverse animation-tmp.gif animation.gif
-#rm -f animation-tmp.gif
+
+# 输出视频
+#cat *.png | ffmpeg -f image2pipe -framerate 10  -i - output2.mkv
 
 
